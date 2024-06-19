ba977a34db25218f98c4a6484b274d7c412a5a8b
Marina Elizarova <seagirl1110@gmail.com>
2024-06-19 23:52:34 +0300

    feat: add submit subscribe-form

--- script/index.js
+++ script/index.js
@@ -1,3 +1,5 @@
+// cookies
+
 const cookies = document.querySelector('.cookies');
 const cookiesBtnColl = cookies.querySelectorAll('.cookies__btn');
 
@@ -13,10 +15,48 @@ cookiesBtnColl.forEach((btn) => {
   });
 });
 
+// faq
+
 const faqColl = document.querySelectorAll('.faq');
 
 faqColl.forEach((elem) => {
   elem.addEventListener('click', (evt) => {
     evt.currentTarget.classList.toggle('faq--active');
   });
+});
+
+// form
+
+const subscribeForm = document.querySelector('.subscribe-form');
+
+const firstNameInput = subscribeForm.querySelector('#firstName');
+const lastNameInput = subscribeForm.querySelector('#lastName');
+const emailInput = subscribeForm.querySelector('#email');
+const subscribeInput = subscribeForm.querySelector('#subscribe');
+
+const subscribeStatus = subscribeForm.querySelector('.subscribe-form__status');
+
+const isValidateForm = () => {
+  return (
+    firstName.value.length &&
+    lastName.value.length &&
+    email.value.length &&
+    subscribe.checked
+  );
+};
+
+const clearForm = () => {
+  firstName.value = '';
+  lastName.value = '';
+  email.value = '';
+  subscribe.checked = false;
+};
+
+subscribeForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  subscribeStatus.textContent = '';
+  if (isValidateForm()) {
+    subscribeStatus.textContent = `${firstName.value} ${lastName.value}, you have successfully subscribed. The newsletter will be sent by email ${email.value}`;
+    clearForm();
+  }
 });
